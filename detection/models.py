f093956434995d09b5d0b40ce447eaaca1cd5d10
pankajrajparajuli <pankajrajparajuli@gmail.com>
2025-07-21 22:40:06 +0545

    added timestamp

--- detection/models.py
+++ detection/models.py
@@ -1,7 +1,7 @@
-
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
+
 
 class PredictionHistory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -10,6 +10,7 @@ class PredictionHistory(models.Model):
     confidence = models.FloatField()
     remedy = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    preventive_measures = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return f"{self.user.username} - {self.disease} - {self.timestamp}"
+        return f"{self.user.username} - {self.disease} - {self.confidence:.2f} - {self.timestamp}"
